2ff37a269d57be0c856f0432d29c056bd26c9c12
Devin Lethbridge <devinbeth@gmail.com>
2018-02-19 16:45:35 -0700

    all wizards working

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,17 +1,19 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { updateRealEstateAgent } from '../../ducks/reducer.js';
 
 class WizardFive extends Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="parent-div">
-                    <div className="vert-align">    
+                <div className="vert-align">
 
                     <p>Are you currently working with a real estate agent?</p> <br />
                     <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
+                        <Link to="/wSix"><button onClick={() => this.props.updateRealEstateAgent('True')}>Yes</button></Link>
+                        <Link to="/wSix"><button onClick={() => this.props.updateRealEstateAgent('False')}>No </button></Link>
                     </div>
                 </div>
             </div>
@@ -19,4 +21,13 @@ class WizardFive extends Component {
     }
 }
 
-export default WizardFive;
+function mapStateToProps(state) {
+    if (!state) {
+        return {};
+    }
+    return {
+        realEstateAgent: state.realEstateAgent
+    };
+}
+
+export default connect(mapStateToProps, { updateRealEstateAgent })(WizardFive);
--- src/components/WizardSix/WizardSix.js
+++ src/components/WizardSix/WizardSix.js
@@ -1,23 +1,18 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { updateCost, updateDownPayment } from '../../ducks/reducer.js';
 
 class WizardSix extends Component {
 
     render(){
         return(
             <div className="parent-div">
-                    <div className="vert-align">                    <p>What is the estimated purchase price?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateCost}/> <br />
-
-
+                <div className="vert-align">
+                    <p>What is the estimated purchase price?</p> <br />
+                    <input type="text" placeholder="amount" onChange={(e) => this.props.updateCost(e.target.value)}/> <br />
                     <p>How much are you putting down as a down payment?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateDownPayment}/>                    
-                        
-                    
+                    <input type="text" placeholder="amount" onChange={(e) => this.props.updateDownPayment(e.target.value)}/>                    
                     <Link to="/wSeven"><button className="margin-btn"> Next </button></Link>
                 </div>
             </div>
@@ -25,4 +20,14 @@ class WizardSix extends Component {
     }
 }
 
-export default WizardSix;
+function mapStateToProps(state) {
+    if (!state) {
+        return {};
+    }
+    return {
+        cost: state.cost,
+        downPayment: state.downPayment
+    };
+}
+
+export default connect(mapStateToProps, { updateCost, updateDownPayment })(WizardSix);
